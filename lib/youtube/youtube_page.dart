18c0997a09b191620c47356670962d3e4aa0ac4a
tai0116 <taisukekacho@gmail.com>
2022-12-21 16:06:40 +0900

    UIのリファクタリング

--- lib/youtube/youtube_page.dart
+++ lib/youtube/youtube_page.dart
@@ -44,153 +44,182 @@ class _YouTubePageState extends State<YouTubePage> {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final halfButtonWidth = (size.width - 16) / 2;
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         title: Image.asset('images/youtube.png'),
-        iconTheme: IconThemeData(color: Colors.white),
-        actions: [
-          Icon(Icons.cast),
-          SizedBox(width: 16),
-          Icon(Icons.notifications_none),
-          SizedBox(width: 16),
-          Icon(Icons.search_rounded),
-          SizedBox(width: 16),
-          IconButton(
-            icon: CircleAvatar(
-              backgroundColor: Colors.purple,
-            ),
-            onPressed: () {},
-          ),
+        iconTheme: const IconThemeData(color: Colors.white),
+        actions: const [
+          _AppBarActions(),
         ],
         backgroundColor: Colors.black,
       ),
-      body: ListView(
-        children: [
-          Column(
+      body: _Body(dummyMovieData: _dummyMovieData),
+      bottomNavigationBar: const _BottomNavigation(),
+    );
+  }
+}
+
+class _AppBarActions extends StatelessWidget {
+  const _AppBarActions({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const <Widget>[
+        Icon(Icons.cast),
+        SizedBox(width: 16),
+        Icon(Icons.notifications_none),
+        SizedBox(width: 16),
+        Icon(Icons.search_rounded),
+        SizedBox(width: 16),
+        IconButton(
+          icon: CircleAvatar(
+            backgroundColor: Colors.purple,
+          ),
+          onPressed: null,
+        ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({Key? key, required this.dummyMovieData}) : super(key: key);
+
+  final List<MovieInfo> dummyMovieData;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Column(
+          children: const [
+            CreateCategoryButton(
+              icon: Icons.local_fire_department_sharp,
+              icon2: Icons.music_note,
+              label: '急上昇',
+              label2: '音楽',
+            ),
+            CreateCategoryButton(
+              icon: Icons.videogame_asset,
+              icon2: Icons.text_snippet,
+              label: 'ゲーム',
+              label2: 'ニュース',
+            ),
+            CreateCategoryButton(
+              icon: Icons.lightbulb,
+              icon2: Icons.wifi_tethering,
+              label: '学び',
+              label2: 'ライブ',
+            ),
+            CreateCategoryButton(
+              icon: Icons.sports_baseball,
+              icon2: null,
+              label: 'スポーツ',
+              label2: '',
+            ),
+          ],
+        ),
+        const _HotVideos(),
+        _HotVideosList(dummyMovieData: dummyMovieData),
+      ],
+    );
+  }
+}
+
+class _HotVideosList extends StatelessWidget {
+  const _HotVideosList({
+    Key? key,
+    required this.dummyMovieData,
+  }) : super(key: key);
+
+  final List<MovieInfo> dummyMovieData;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: dummyMovieData.length,
+      itemBuilder: (context, index) {
+        return SizedBox(
+          height: 320,
+          child: Column(
             children: [
-              createCategoryButton(
-                halfButtonWidth,
-                Icons.local_fire_department_sharp,
-                Icons.music_note,
-                '急上昇',
-                '音楽',
-              ),
-              createCategoryButton(
-                halfButtonWidth,
-                Icons.videogame_asset,
-                Icons.text_snippet,
-                'ゲーム',
-                'ニュース',
-              ),
-              createCategoryButton(
-                halfButtonWidth,
-                Icons.lightbulb,
-                Icons.wifi_tethering,
-                '学び',
-                'ライブ',
-              ),
-              createCategoryButton(
-                halfButtonWidth,
-                Icons.sports_baseball,
-                Icons.music_note,
-                'スポーツ',
-                '',
+              Image.network(dummyMovieData[index].imagePath),
+              ListTile(
+                title: Text(
+                  dummyMovieData[index].title,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
+                  ),
+                ),
+                subtitle: Text(
+                  dummyMovieData[index].subTitle,
+                  style: const TextStyle(
+                    fontSize: 12,
+                    color: Colors.white,
+                  ),
+                ),
+                leading: CircleAvatar(
+                  radius: 22,
+                  child: Image.asset(dummyMovieData[index].iconPath),
+                ),
+                trailing: const Icon(
+                  Icons.more_vert,
+                  color: Colors.white,
+                ),
               ),
             ],
           ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            child: Text(
-              '急上昇動画',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: _dummyMovieData.length,
-            itemBuilder: (context, index) {
-              return Container(
-                height: 300,
-                child: Column(
-                  children: [
-                    Image.network(_dummyMovieData[index].imagePath),
-                    ListTile(
-                      title: Text(
-                        _dummyMovieData[index].title,
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.white,
-                        ),
-                      ),
-                      subtitle: Text(
-                        _dummyMovieData[index].subTitle,
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.white,
-                        ),
-                      ),
-                      leading: CircleAvatar(
-                        radius: 22,
-                        child: Image.asset(_dummyMovieData[index].iconPath),
-                      ),
-                      trailing: Icon(
-                        Icons.more_vert,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.black,
-        type: BottomNavigationBarType.fixed,
-        showUnselectedLabels: true,
-        unselectedItemColor: Colors.white,
-        selectedItemColor: Colors.white,
-        unselectedFontSize: 10,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'ホーム',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: '検索',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_outline),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.video_library_sharp),
-            label: 'チャンネル',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.video_library_sharp),
-            label: 'ライブラリ',
-          ),
-        ],
+        );
+      },
+    );
+  }
+}
+
+class _HotVideos extends StatelessWidget {
+  const _HotVideos({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: const Text(
+        '急上昇動画',
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.white,
+        ),
       ),
     );
   }
+}
 
-  Widget createCategoryButton(halfButtonWidth, IconData icon, IconData icon2,
-      String label, String label2) {
+class CreateCategoryButton extends StatelessWidget {
+  const CreateCategoryButton({
+    Key? key,
+    this.icon,
+    this.icon2,
+    this.label,
+    this.label2,
+  }) : super(key: key);
+
+  final IconData? icon;
+  final IconData? icon2;
+  final String? label;
+  final String? label2;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final halfButtonWidth = (size.width - 16) / 2;
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         Container(
-          padding: EdgeInsets.fromLTRB(0, 5, 5, 5),
+          padding: const EdgeInsets.fromLTRB(0, 5, 5, 5),
           width: halfButtonWidth,
           height: 55,
           child: ElevatedButton(
@@ -201,14 +230,14 @@ class _YouTubePageState extends State<YouTubePage> {
             child: Row(
               children: [
                 Icon(icon),
-                SizedBox(width: 8),
-                Text(label),
+                const SizedBox(width: 8),
+                Text(label ?? ''),
               ],
             ),
           ),
         ),
         Container(
-          padding: EdgeInsets.fromLTRB(0, 5, 5, 5),
+          padding: const EdgeInsets.fromLTRB(0, 5, 5, 5),
           width: halfButtonWidth,
           height: 55,
           child: ElevatedButton(
@@ -219,11 +248,49 @@ class _YouTubePageState extends State<YouTubePage> {
             child: Row(
               children: [
                 Icon(icon2),
-                SizedBox(width: 8),
-                Text(label2),
+                const SizedBox(width: 8),
+                Text(label2 ?? ''),
               ],
             ),
           ),
+        ),
+      ],
+    );
+  }
+}
+
+class _BottomNavigation extends StatelessWidget {
+  const _BottomNavigation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: Colors.black,
+      type: BottomNavigationBarType.fixed,
+      showUnselectedLabels: true,
+      unselectedItemColor: Colors.white,
+      selectedItemColor: Colors.white,
+      unselectedFontSize: 10,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'ホーム',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: '検索',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_circle_outline),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.video_library_sharp),
+          label: 'チャンネル',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.video_library_sharp),
+          label: 'ライブラリ',
         ),
       ],
     );
